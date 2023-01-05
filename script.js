c8bc66ed25f7c68c6f2f25c1ddfdd34900198e2c
Annie Guo <anniewenqingguo@gmail.com>
2023-01-04 19:08:35 -0500

    Implementing MediaPipe to draw out Hand Landmarks

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-let video = document.querySelector('#webcam')
-if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia( { video: true }).then(function (stream) {
-        video.srcObject = stream;
-    }).catch(function (error) {
-        console.log("Error: Something went wrong")
-    })
-} else {
-    console.log("Error: Unable to retrieve webcam data, getUserMedia unsupported");
+const videoElement = document.getElementsByClassName('input')[0];
+const canvasElement = document.getElementsByClassName('output')[0];
+const canvasCtx = canvasElement.getContext('2d');
+
+function onResults(results) {
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(
+      results.image, 0, 0, canvasElement.width, canvasElement.height);
+  if (results.multiHandLandmarks) {
+    for (const landmarks of results.multiHandLandmarks) {
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+                     {color: '#4b2503', lineWidth: 2});
+      drawLandmarks(canvasCtx, landmarks, {color: '#4b2503', lineWidth: 1});
+    }
+  }
+  canvasCtx.restore();
 }
+
+const hands = new Hands({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+}});
+hands.setOptions({
+  maxNumHands: 2,
+  modelComplexity: 1,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+hands.onResults(onResults);
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await hands.send({image: videoElement});
+  },
+  width: 320,
+  height: 180
+});
+camera.start();
