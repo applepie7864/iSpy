e54ad9a4526a33fbe02977694f4b1d16ca59015f
Annie Guo <anniewguo@Annies-MacBook-Air-2.local>
2023-09-14 14:03:24 -0400

    setup webcam

--- face_detect.py
+++ face_detect.py
@@ -1,47 +1,20 @@
+import numpy as np
 import cv2
-import sys
 
-# implementing a face cascade
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') 
+# set video source to default webcam
+cam = cv2.VideoCapture(0)
+cam.set(3, 1280) # set width
+cam.set(4, 720) # set height
 
-# sets video source to default webcam
-video_capture = cv2.VideoCapture(0)
-
-# captures the video one frame at a time
 while True:
-    ret, frame = video_capture.read()
-
-    # converts color of the frame to bgr2gray model
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Note: detectMultiScale function is used to detect 
-    # the faces. This function will return a list of 
-    # rectangles (x,y,w,h) around the detected objects.
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5, 
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
-
-    # Draw a rectangle on indentified faces given the coordinates onto frame
-    # Usage: rectangle(start_point, end_point, color, thickness)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    ret, frame = cam.read() # extracting a camera frame
+    cv2.imshow('frame', frame) # displaying the frame
     
-    # Display result
-    cv2.imshow('Video', frame)
-
-    # if the q key is pressed, we quit and exit the while loop
-    # Usage: ord(character) extracts the unicode value of a character
-    #        cv2.waitkey(delay) waits for a key to be pressed for delay seconds
-    #                           and if a key is pressed, it returns a 32-bit integer
-    #                           corresponding to the key
-    #        & 0xFF is a bit mask which sets the left 24 bits to zero so that 
-    #        the above two function return values can make a valid comparison
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # press 'ESC' to quit
+    key = cv2.waitKey(30) & 0xff
+    if key == 27:
         break
 
-# Cleaning up once program is finishedvideo_capture.release()
+# clean up
+cam.release()
 cv2.destroyAllWindows()
