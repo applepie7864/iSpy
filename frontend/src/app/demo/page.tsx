29cc4df0c8a71b653720277e6ac223b45264e4f0
applepie7864 <anniewenqingguo@gmail.com>
2023-11-19 18:05:00 -0500

    demo

--- frontend/src/app/demo/page.tsx
+++ frontend/src/app/demo/page.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import Link from 'next/link'
 import NavBar from '@/app/components/navbar'
 import Footer from '@/app/components/footer'
+import '@/app/styles/demo.css'
 
 const demo = () => {
   return (
     <div>
         <NavBar />
+        <div className="screen">
+            <div className="camera-wrapper">
+                <div className="webcam"></div>
+            </div>
+        </div>
         <Footer />
     </div>
   )
